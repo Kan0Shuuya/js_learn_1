ba132f848a39d4ea951275712dc3b8d833afc5e5
Yura <nastasya.ustinskaya@bk.ru>
2021-02-07 19:28:47 +0600

    Добвывил файлов на будущее

---
+++ My projects and tests/5 test/script.js
@@ -0,0 +1,16 @@
+"use strict"
+let personalMovieDB = {
+    count  : prompt("Сколько фильмов вы уже посмотрели?",9),
+    movies : {},
+    actors : {},
+    genret : [],
+    privat : confirm("Будет ли приватен ли ваш аккаунт?")
+};
+let a = prompt("Один из последних просмотренных фильмов?"),
+    b = prompt("На сколько оцените его?"),
+    c = prompt("Один из последних просмотренных фильмов?"),
+    d = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB.movies)
