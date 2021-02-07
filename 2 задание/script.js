1584d3c1996b559e49ec348711435bcf01208e2e
Kan0Shuuya <nastasya.ustinskaya@bk.ru>
2021-02-07 12:39:20 +0600

    2 Проект сделан до конца

--- 2 задание/script.js
+++ 2 задание/script.js
@@ -9,11 +9,16 @@ let personalMovieDB = {
         genret : [],
         privat : confirm("Будет ли приватен ли ваш аккаунт?")
 };
-
 for (; i !== x; x++) {
-    let a = prompt(`Введите название одного из просмотренных вами, фильмов,
-     который вы, можете оценить`),
-        b = prompt("На сколько вы его оценили бы?");
-    personalMovieDB.movies[a] = b;
-};
-console.log(personalMovieDB.movies)
+        let a = prompt(`Введите название одного из просмотренных вами, фильмов, который вы, можете оценить`),
+            b = prompt("На сколько вы его оценили бы?");
+        
+            if( a != null && b != null && a != '' && b != '' && a.length <50){
+            personalMovieDB.movies[a] = b;
+            console.log('done')
+        } else{
+            console.log("error")
+            x--;
+        }
+    };
+console.log(personalMovieDB)
