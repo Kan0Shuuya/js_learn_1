b24fc36d388f67b28372d2b493784908c9e43d8b
Kan0Shuuya <nastasya.ustinskaya@bk.ru>
2021-02-07 12:11:09 +0600

    Сделан 2 проект

--- 2 задание/script.js
+++ 2 задание/script.js
@@ -1,16 +1,19 @@
 "use strict"
-let personalMovieDB = {
-    count  : prompt("Сколько фильмов вы уже посмотрели?",9),
-    movies : {},
-    actors : {},
-    genret : [],
-    privat : confirm("Будет ли приватен ли ваш аккаунт?")
-};
-let a = prompt("Один из последних просмотренных фильмов?"),
-    b = prompt("На сколько оцените его?"),
-    c = prompt("Один из последних просмотренных фильмов?"),
-    d = prompt("На сколько оцените его?");
+var i = +prompt("Сколько фильмов вы смотрели?"),
+    x = 0;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+let personalMovieDB = {
+        count  : i,
+        movies : {},
+        actors : {},
+        genret : [],
+        privat : confirm("Будет ли приватен ли ваш аккаунт?")
+};
+
+for (; i !== x; x++) {
+    let a = prompt(`Введите название одного из просмотренных вами, фильмов,
+     который вы, можете оценить`),
+        b = prompt("На сколько вы его оценили бы?");
+    personalMovieDB.movies[a] = b;
+};
 console.log(personalMovieDB.movies)
