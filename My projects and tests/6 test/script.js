04d7081a596b9facac417686333865f4d6e6b84d
Yura <nastasya.ustinskaya@bk.ru>
2021-02-22 14:44:57 +0600

    а

---
+++ My projects and tests/6 test/script.js
@@ -0,0 +1,13 @@
+"use strict"
+let i = +prompt("Введите число")
+switch (i) {
+    case i=9:
+        console.log(9)
+        break;
+    case i < 9:
+        console.log("Меньше 9")
+        break;
+    default:
+        console.log("Не равно 9, и не меньше 9")
+        break;
+}
--- Проекты/Циклы/script.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-for (let a = 1, b = 1, c=0;b != 9 || a != 11; a++) {
-    b = a == 11 ? ++b : b=b;
-    a = a == 11 ? 1 : a;
-    c = a*b
-    console.log(`${a} * ${b} = ${c}`)
-}
-
