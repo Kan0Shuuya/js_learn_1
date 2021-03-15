dd2106bec92099120a534ac18c059673eee85460
Yura <nastasya.ustinskaya@bk.ru>
2021-03-15 22:16:46 +0600

    Доедлал проект

--- My projects and tests/7 test/script.js
+++ My projects and tests/7 test/script.js
@@ -1,1 +1,4 @@
 "use strict";
+(function (x,y) {
+    console.log(x+y)
+}(1,2))
--- My projects and tests/8 test/script.js
+++ My projects and tests/8 test/script.js
@@ -2,21 +2,24 @@
 let z,
     bill =[10000,5000,2000,1000,500,200,100,50,20,10,5,2,1],
     quantity = [],
-    inputCash = 500, //+prompt("Введите, сколько деняг вы дали.", 500)
-    costProduct = 235, //+prompt("Сколько вышло в чеке?", 235)
+    inputCash = +prompt("Введите, сколько деняг вы дали.", 500),
+    costProduct = +prompt("Сколько вышло в чеке?", 235),
     deliveryCash = inputCash-costProduct;
 for (let i of bill) {
     while (true) {
         if(deliveryCash >= i){
             deliveryCash = deliveryCash-i
-            ++z
-            quantity.push(`Наминал ${i}: ${z} кол-во`)
+            ++z    
         }
         else{
+            if (z == 0 || "undefined") {
+                break
+            } else{
+            quantity.push(`Наминал ${i}: ${z} кол-во`)
             z = 0
             break
+            }
         }
     }
 }
 console.log(quantity)
-console.log(deliveryCash)
